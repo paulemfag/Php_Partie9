b659e37f7f624152847d9737ad5cc8ebb7cc0860
paulemfag <boiteprofagot@gmail.com>
2019-12-20 14:19:46 +0100

    modif

--- ex3.php
+++ ex3.php
@@ -3,6 +3,6 @@
   include 'header.php';
 ?>
 <p>Nous sommes le : <?= date('l d F y'); ?></p>
-<?php setlocale(LC_TIME, "fr_FR"); ?>
+<?php setlocale(LC_TIME, 'fr_FR.utf8','fra'); ?>
 <p>Soit le : <?= strftime("%A %d %B %G"); ?></p>
 <?php include 'footer.php'; ?>
