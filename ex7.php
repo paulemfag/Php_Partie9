6ae6450b865282d344fe834d8b74fd9801a69b90
paulemfag <boiteprofagot@gmail.com>
2019-12-20 12:02:08 +0100

    modifs

--- ex7.php
+++ ex7.php
@@ -1,7 +1,7 @@
 <?php
   $page = 'Exercice 7';
   include 'header.php';
-    $date = new DateTime('18-12-2019');
+    $date = new DateTime('now');
     $date->add(new DateInterval('P20D'));
 ?>
 <p><?= 'Si on ajoute 20 jours à la date d\'aujourd\'hui nous obtenons : '.  $date->format('d-m-Y') . "\n"; ?></p>
--- ex8.php
+++ ex8.php
@@ -1,7 +1,7 @@
 <?php
   $page = 'Exercice 8';
   include 'header.php';
-$date = new DateTime('18-12-2019');
+$date = new DateTime('now');
 $date->sub(new DateInterval('P22D'));
 ?>
 <p><?= 'Si on enlève 22 jours à la date d\'aujourd\'hui nous obtenons : '. $date->format('d-m-Y') . "\n"; ?></p>
